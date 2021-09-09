ed989bd43fe263722c181e2954cdca6245dd1466
Collin-5 <collinsotieno380@gmail.com>
2021-09-09 14:16:09 +0300

    hidden

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    list = dir(hidden_4)
-    for i in range(0, len(list)):
-        if list[i][0:2] != "__":
-            print(list[i])
+    for i in dir(hidden_4):
+        if not i.startswith("__"):
+            print("{:s}".format(i))
