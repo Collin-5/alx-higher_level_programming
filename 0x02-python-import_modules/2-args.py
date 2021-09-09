83e24adbcba0a17861ada30c526f491a52e8acfd
Collin-5 <collinsotieno380@gmail.com>
2021-09-09 13:14:55 +0300

    sys argv

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    arguments = argv[1:]
+    elements = len(arguments)
+    if elements == 0:
+        print("{} arguments.".format(elements))
+    elif elements == 1:
+        print("{} argument".format(elements))
+        print("{}: {}".format(elements, arguments[0]))
+    else:
+        print("{} arguments".format(elements))
+        for i in range(0, elements):
+            print("{}: {}".format(i, arguments[i]))
