bd6018b41937ab27f61f06807c55c47a1110a75f
Collin-5 <collinsotieno380@gmail.com>
2021-10-04 12:10:58 +0300

    making executables

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """
-Module 2-rectangle
+Module 3-rectangle
 Contains class Rectangle with private attribute width and height,
-and public area and perimeter methods
+public area and perimeter methods, and allows printing #'s
 """
 
 
@@ -20,6 +20,7 @@ class Rectangle:
         height(self, value)
         area(self)
         perimeter(self)
+        __str__(self)
     """
     def __init__(self, width=0, height=0):
         """ Initialize rectangles """
@@ -63,3 +64,10 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return 0
         return (2 * self.__width) + (2 * self.height)
+
+    def __str__(self):
+        """ Prints rectangle with #'s """
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        pic = "\n".join(["#" * self.__width for rows in range(self.__height)])
+        return pic
