7ee408303756532a9f25083243cf5fbe26ff3bdb
Collin-5 <collins380@gmail.com>
2021-11-16 16:56:14 +0300

    insert into a table

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,3 @@
--- Instert to a table
+-- Instert a row into a table
 
-INSERT INTO first_table (id, name) VALUES (89, 'Best School');
+INSERT INTO `first_table` (id, name) VALUES (89, 'Best School');
