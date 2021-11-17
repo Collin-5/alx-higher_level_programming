377ebd64d6230199e15bd2d09cba21428b452c9d
Collin-5 <collins380@gmail.com>
2021-11-17 14:12:37 +0300

    crete database user and pr

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,8 @@
+-- create database
+-- crete user with select privilages
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT USAGE ON *.* TO 'user_0d_2'@'localhost';
+GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
