d21b62d2b6e335e4414d7d9c9ebb1a5b3ab3556c
Collin-5 <collins380@gmail.com>
2021-11-17 15:22:26 +0300

    gener

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,8 @@
+-- list with gener
+
+SELECT tv_genres.name AS genre, COUNT(*) AS 'number_shows'
+      FROM tv_genres
+INNER JOIN tv_show_genres
+        ON tv_genres.id = tv_show_genres.genre_id
+  GROUP BY genre
+  ORDER BY number_shows DESC;
