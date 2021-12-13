f838f97c81b5d93fee3171613dde12dd70ee09bb
Collin-5 <collins380@gmail.com>
2021-12-13 13:45:39 +0300

    addition

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,5 +1,8 @@
 #!/usr/bin/node
 
-function add(a, b) {
-  console.log(a + b);
+const a = parseInt(process.argv[2], 10);
+const b = parseInt(process.argv[3], 10);
+function add (x, y) {
+  return (x + y);
 }
+console.log(add(a, b));
