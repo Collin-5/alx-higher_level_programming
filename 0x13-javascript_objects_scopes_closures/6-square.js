6687a48042d4406df928ac44d0a10fa888808963
Collin-5 <collins380@gmail.com>
2021-12-14 14:17:36 +0300

    print with c

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -7,7 +7,7 @@ module.exports = class Square extends BaseSquare {
       this.print();
     } else {
       for (let i = 0; i < this.width; i += 1) {
-	console.log(c.repeat(this.height));
+        console.log(c.repeat(this.height));
       }
     }
   }
