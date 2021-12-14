3fb691442210a917e1b6d875dce2818f3bbe9f0a
Collin-5 <collins380@gmail.com>
2021-12-14 13:47:20 +0300

    print

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -7,8 +7,9 @@ module.exports = class Rectangle {
       this.height = h;
     }
   }
+
   print () {
-    for (let col = 0; col < this.height; col += 1) {
+    for (let i = 0; i < this.height; i += 1) {
       console.log('X'.repeat(this.width));
     }
   }
