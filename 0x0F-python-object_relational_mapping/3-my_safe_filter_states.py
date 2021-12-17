a2ddbab63f56e97cee34639d6e309bc4e8dcac6a
Collin-5 <collins380@gmail.com>
2021-12-17 22:28:24 +0300

    safe sql

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+return all table thtat looks like user input
+parameters given to script: username, password, database
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+
+    # connect to database
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+
+    # create cursor to exec queries using SQL
+    c = db.cursor()
+    mcmd = """SELECT * FROM states WHERE name=%s ORDER BY id ASC"""
+    c.execute(mcmd, (argv[4],))
+    for row in c.fetchall():
+        print(row)
+    c.close()
+    db.close()
