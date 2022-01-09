7b49dfbdfb1f1a4bf62ced65a4d5fa06611789b2
Collin-5 <collins380@gmail.com>
2022-01-09 22:56:32 +0300

    post request letter

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-given letter as param, POST to http://0.0.0.0:5000/search_user
+given letter as param,POST to http://0.0.0.0:5000/search_user
 usage: ./8-json_api.py [letter only]
 """
 from sys import argv
