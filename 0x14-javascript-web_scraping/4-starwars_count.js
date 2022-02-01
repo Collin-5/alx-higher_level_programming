023b52e16872c14ef27a5bfc42c8fcde3d1487b3
Collin-5 <collins380@gmail.com>
2022-02-01 14:01:14 +0300

    count

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+const url = process.argv[2];
+const request = require('request');
+
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    const films = JSON.parse(body).results;
+    let count = 0;
+    for (const i in films) {
+      const chars = films[i].characters;
+      for (const c in chars) {
+        if (chars[c].includes('18')) {
+          count++;
+        }
+      }
+    }
+    console.log(count);
+  } else {
+    console.log('Erorr Code:' + response.statusCode);
+  }
+});
