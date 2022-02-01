18e23ead8bc3c0f2e0a772577c2bdda4df76a192
Collin-5 <collins380@gmail.com>
2022-02-01 16:09:19 +0300

    character

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,24 +1,25 @@
 #!/usr/bin/node
+const request = require('request');
 
 const id = process.argv[2];
 const url = `https://swapi-api.hbtn.io/api/films/${id}`;
-const request = require('request');
 
-request(url, function (err, response, body) {
-  if (err) {
-    console.log(err);
-  } else if (response.statusCode === 200) {
-    body = JSON.parse(body);
-    for (const i in body.characters) {
-      request(body.characters[i], function (err, response, body) {
-        if (err) {
-          console.log(err);
-        } else if (response.statusCode === 200) {
-          console.log(JSON.parse(body).name);
+request.get(url, async function (error, response, body) {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  const data = JSON.parse(body);
+  for (const character of data.characters) {
+    await new Promise(function (resolve, reject) {
+      request(character, function (error, response, body) {
+        if (error) {
+          reject(error);
+          return;
         }
+        console.log(JSON.parse(body).name);
+        resolve();
       });
-    }
-  } else {
-    console.log('Erorr Code:' + response.statusCode);
+    });
   }
 });
