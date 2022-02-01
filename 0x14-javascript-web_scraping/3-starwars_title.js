79bf3f2243523b3fe697962f975d7e095fedd51d
Collin-5 <collins380@gmail.com>
2022-02-01 13:39:06 +0300

    swapi

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 const id = process.argv[2];
-const url = 'http://swapi.co/api/films/' + id;
+const url = 'https://swapi-api.hbtn.io/api/films/:id';
 const request = require('request');
 
 request(url, function (err, response, body) {
