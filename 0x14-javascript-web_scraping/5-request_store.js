406418f9d1893f8c556cd9d1563ea55dff68e441
Collin-5 <collins380@gmail.com>
2022-02-01 14:17:39 +0300

    html content

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -9,6 +9,10 @@ request(url, function (err, response, body) {
   if (err) {
     console.log(err);
   } else {
-    fs.writeFile(filename, body, 'utf8');
+    fs.writeFile(filename, body, 'utf8', function (err) {
+      if (err) {
+        console.log(err);
+      }
+    });
   }
 });
