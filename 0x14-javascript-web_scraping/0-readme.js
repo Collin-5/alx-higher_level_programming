055125ef62aba55e44d09adbc09532a7cc0aa716
Collin-5 <collins380@gmail.com>
2022-02-01 12:49:30 +0300

    fix

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
-let filename = process.argv[2];
-const rqst = require('rqst');
-rqst.readFile(filename, 'utf8', function (err, data) {
+const filename = process.argv[2];
+const fs = require('fs');
+fs.readFile(filename, 'utf8', function (err, data) {
   if (err) {
     console.log(err);
   } else {
     console.log(data);
   }
-})
+});
